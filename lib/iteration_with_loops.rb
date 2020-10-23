28e06744f81763354476857e1e4a8418a7ce86ea
Nhon Dang <dang.nhon12345@yahoo.com>
2020-10-23 18:27:15 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,7 @@ def find_even_values(src)
     element_index = 0
     while element_index < src[row_index].length do
       puts src[row_index][element_index]
-      element_index += 2
+      element_index += 1
     end
     row_index +=1
 end
